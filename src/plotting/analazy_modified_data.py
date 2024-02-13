7f63e1ae5b4696bb71768e8a5c5e12644d1759e2
LukaszSzarecki <s179980@student.pg.edu.pl>
2024-02-13 23:28:43 +0100

    dzi

--- src/plotting/analazy_modified_data.py
+++ src/plotting/analazy_modified_data.py
@@ -6,10 +6,12 @@ import glob
 project_dir = os.path.dirname(os.path.abspath(__file__))
 print(project_dir)
 
-csv_valence_classes = os.path.join(project_dir, "valance_classes_csv")
+csv_valence_classes = os.path.join(project_dir, "valence_classes_csv")
 csv_path_list_valence_classes = glob.glob(csv_valence_classes + '/*')
 csv_eda_peaks = os.path.join(project_dir, "EDA_peaks_determined_0_1_csv")
 csv_path_list_eda_peaks = glob.glob(csv_eda_peaks + '/*')
+
+y = 2
 
 
 def find_shortest_sequence(series):
@@ -70,15 +72,39 @@ def create_svm_dataset():
         extended_valance = df_valence.reindex(df_valence.index.repeat(40))
         extended_valance = extended_valance.reset_index(drop=True)
         extended_valance = extended_valance.drop(extended_valance.index[-40:])
-        df_eda["labels"] = extended_valance["classes"]
+        df_eda["classes"] = extended_valance["classes"]
         svm_path = valance_path.replace('valance_classes_csv', 'svm_dataset')
         df_eda.to_csv(svm_path, index=False, sep=';')
+
+
+def create_nn_dataset():
+    for eda_path, valance_path in zip(csv_path_list_eda_peaks, csv_path_list_valence_classes):
+        df_valence = pd.read_csv(valance_path, delimiter=';')
+
+        df_eda = pd.read_csv(eda_path, delimiter=';')
+
+        df_valence["EDA"] = None
+        df_valence["is_peak"] = None
+        for index, row in df_valence.iterrows():
+            eda_array = df_eda.loc[index * 40:(index + 1) * 40 - 1, 'EDA'].tolist()
+            # is_peak_array = df_eda.loc[index * 40:(index + 1) * 40 - 1, 'is_peak'].tolist()
+            df_valence.at[index, 'EDA'] = eda_array
+            # df_valence.at[index, 'is_peak'] = is_peak_array
+            try:
+                is_peak_value = df_eda.loc[index * 40, 'is_peak']
+                df_valence.at[index, 'is_peak'] = is_peak_value
+            except Exception as e:
+                pass
+
+        nn_path = valance_path.replace('valence_classes_csv', 'nn_dataset')
+        df_valence.to_csv(nn_path, index=False, sep=';')
 
 
 def main():
     # bool_to_int_is_peak()
     # drop_unnecessary_columns()
-    create_svm_dataset()
+    # create_svm_dataset()
+    create_nn_dataset()
 
 
 if __name__ == "__main__":
